60098a6ebe490ca62757f4b42b6b13ef0e89fd4d
Lance Larsen <lancelarsen2001@gmail.com>
2022-10-19 13:37:31 -0600

    store setup completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Header from './Containers/Header'
+import ProductListing from './Containers/ProductListing';
+import ProductDetail from './Containers/ProductDetail';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Header />
+        <Routes>
+
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:prodectId" exact component={ProductDetail} />
+          <Route>404 Not Found</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
